afe043b232ed8b2e455923b81b764abf1131e0d5
yevheniia.erkan <yevheniia.erkan@bzwu.ch>
2026-05-26 11:25:05 +0200

    Added automatic dark/light mode and improved code structure

--- Hauptseite.js
+++ Hauptseite.js
@@ -7,10 +7,9 @@ navLinks.forEach(link => {
     e.preventDefault();
 
     const targetID = this.getAttribute("href");
-
     const targetSection = document.querySelector(targetID);
 
-    if(targetSection){
+    if (targetSection) {
 
       targetSection.scrollIntoView({
         behavior: "smooth"
@@ -26,12 +25,10 @@ navLinks.forEach(link => {
 /* SLIDER */
 
 const slider = document.querySelector(".slider");
-
 const next = document.querySelector(".next");
-
 const prev = document.querySelector(".prev");
 
-if(slider && next && prev){
+if (slider && next && prev) {
 
   next.addEventListener("click", () => {
 
@@ -44,5 +41,14 @@ if(slider && next && prev){
     slider.scrollLeft -= 450;
 
   });
+
+}
+
+
+/* LIGHT / DARK MODE */
+
+if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+
+  document.body.classList.add("light-mode");
 
 }
